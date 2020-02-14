89e20cedf3a77fb9dfaf05e5be61b7572700ad00
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-02-14 16:32:33 -0300

    Adicionar funcionalidades de fatura e lancamentos

--- src/main/java/br/itau/com/modulo2revisao206/models/Fatura.java
+++ src/main/java/br/itau/com/modulo2revisao206/models/Fatura.java
@@ -1,4 +1,25 @@
 package br.itau.com.modulo2revisao206.models;
 
+import java.math.BigDecimal;
+
 public class Fatura {
+    private BigDecimal total;
+
+    private Iterable<Lancamento> lancamentos;
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Iterable<Lancamento> getLancamentos() {
+        return lancamentos;
+    }
+
+    public void setLancamentos(Iterable<Lancamento> lancamentos) {
+        this.lancamentos = lancamentos;
+    }
 }
