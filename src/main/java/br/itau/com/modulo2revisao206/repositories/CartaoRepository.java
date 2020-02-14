696136a601fb9758e10302878b6f6879cb65c130
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-02-14 16:56:22 -0300

    Proposta de estudo

---
+++ src/main/java/br/itau/com/modulo2revisao206/repositories/CartaoRepository.java
@@ -0,0 +1,6 @@
+package br.itau.com.modulo2revisao206.repositories;
+
+import br.itau.com.modulo2revisao206.models.Cartao;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartaoRepository extends CrudRepository<Cartao, String> {}
