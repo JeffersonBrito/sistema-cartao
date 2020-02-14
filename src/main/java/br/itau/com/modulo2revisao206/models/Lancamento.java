c08effedbf6c6c813c93c1375f7119175345122a
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-02-14 16:53:15 -0300

    Adicionar funcionalidade de fatura e lancamentos

--- src/main/java/br/itau/com/modulo2revisao206/models/Lancamento.java
+++ src/main/java/br/itau/com/modulo2revisao206/models/Lancamento.java
@@ -1,4 +1,57 @@
 package br.itau.com.modulo2revisao206.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
 public class Lancamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @DecimalMin(value = "0.01")
+    @NotNull
+    private String descricao;
+    private LocalDateTime horario;
+
+    @ManyToOne
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Cartao cartao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+
+    public Cartao getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(Cartao cartao) {
+        this.cartao = cartao;
+    }
 }
